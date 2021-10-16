c3577ecd124d12f1104d9dc44b5c809ccfe5304d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-16 11:25:04 -0300

    added typeorm,sqlite3 and pg

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  birth_date: Date;
+
+  @Column()
+  cpf: string;
+}

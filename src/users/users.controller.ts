529fd053709b972fa39e2dde2a983579b6cf4709
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-10-19 18:26:49 -0300

    done auth on user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,12 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 

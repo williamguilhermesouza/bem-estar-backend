bc3a085745bdc895defe52b5783c6edb32f26db1
williamguilhermesouza <williamguilhermesouza@gmail.com>
2021-11-02 23:46:52 -0300

    added password encryption

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +12,7 @@ export class AuthService {
 
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
-    const passwordMatch = bcrypt.compareSync(pass, user.password);
+    const passwordMatch = await bcrypt.compare(pass, user.password);
     if (user && passwordMatch) {
       const { password, ...result } = user;
       return result;
